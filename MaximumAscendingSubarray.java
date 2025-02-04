bc86d3c4d3686e2625001597e12bf17ebd1b63ea
Rishabh Dosi <rishabh3dosi@gmail.com>
2025-02-04 22:07:27 +0530

    maximum ascending subarray sum

---
+++ MaximumAscendingSubarray.java
@@ -0,0 +1,15 @@
+public class MaximumAscendingSubarray {
+    public int maxAscendingSum(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                sum = sum + nums[i];
+                max = Math.max(max, sum);
+            } else {
+                sum = nums[i];
+            }
+        }
+        return max;
+    }
+}
